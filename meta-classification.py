aa63a46c8b3cf58e0f05ce9ce6e8d0358f9587f2
Alexey Antonov <alant4741@mail.ru>
2017-02-22 00:16:31 +0300

    Train/test split is before data preparation and trees for each category are ready

--- meta-classification.py
+++ meta-classification.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 # Author: Aliaksei Antonau (alant4741@mail.ru)
 import psycopg2
+from itertools import *
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -29,12 +30,23 @@ for site in sites_features:
     all_features.append(to_int)
 # print all_features
 
-# Формируем таблицы 5000х50 признаков для каждой категории
-# То есть для категории есть только её признаки у каждого из 5К сайтов
+# Выбираем категории сайтов из таблицы dat
+cursor.execute("SELECT categ_basis FROM dat_files;")
+
+# Сохраняем категорию каждого сайта в список (длиной 5000)
+categ_vals = [row[0][20:] for row in cursor]
+
+#################################################
+# Разбиваем всю выборку на тренировочный и тестовый наборы
+X_train, X_test, y_train, y_test = train_test_split(
+    all_features, categ_vals, test_size=0.2, stratify=categ_vals)
+
+# Формируем таблицы 4000х50 признаков для каждой категории
+# То есть для категории есть только её признаки у каждого из 4К сайтов
 adult_feat, alco_feat, ecomrc_feat, med_feat, relig_feat = [], [], [], [], []
 feat_list = [adult_feat, alco_feat, ecomrc_feat, med_feat, relig_feat]
 
-for cur in all_features:
+for cur in X_train:
     adult_feat.append(cur[:50])
     alco_feat.append(cur[50:100])
     ecomrc_feat.append(cur[100:150])
@@ -43,19 +55,13 @@ for cur in all_features:
 
 # print ecomrc_feat
 
-# Выбираем категории сайтов из таблицы dat
-cursor.execute("SELECT categ_basis FROM dat_files;")
-
-# Сохраняем категорию каждого сайта в список (длиной 5000)
-categ_vals = [row[0][20:] for row in cursor]
-
 # Инициализация списков для хранения категорий
 adult_categ, alco_categ, ecomrc_categ = [], [], []
 med_categ, relig_categ = [], []
 categ_list = [adult_categ, alco_categ, ecomrc_categ, med_categ, relig_categ]
 
 # Для каждой категории определяем 2 класса
-for cat in categ_vals:
+for cat in y_train:
     if cat == "adult":
         adult_categ.append(cat)
     else:
@@ -84,25 +90,24 @@ for cat in categ_vals:
 # print adult_categ, alco_categ, ecomrc_categ, med_categ, relig_categ
 
 ###############################################
+# Обучение деревьев решений для каждой категории
+# В качестве классификатора выбрано Дерево решений
+clf = tree.DecisionTreeClassifier(min_samples_leaf=30)
+
+# Обучение классификатора для каждой из категорий
+adult_tree = clf.fit(adult_feat, adult_categ)
+alco_tree = clf.fit(alco_feat, alco_categ)
+ecomrc_tree = clf.fit(ecomrc_feat, ecomrc_categ)
+med_tree = clf.fit(med_feat, med_categ)
+relig_tree = clf.fit(relig_feat, relig_categ)
+
+"""
 adult_pred, alco_pred, ecomrc_pred = [], [], []
 med_pred, relig_pred = [], []
 pred_list = [adult_pred, alco_pred, ecomrc_pred, med_pred, relig_pred]
-
-for feat, categ, pred in zip(feat_list, categ_list, pred_list):
-    # Разбиение на обучающие и тестовые выборки
-    X_train, X_test, y_train, y_test = train_test_split(
-        feat, categ, test_size=0.2, stratify=categ)
-
-    # В качестве классификатора выбрано Дерево решений
-    clf = tree.DecisionTreeClassifier(min_samples_leaf=30)
-    # Обучение классификатора
-    clf = clf.fit(X_train, y_train)
-    # Вероятностная классификация
-    predicted = clf.predict(X_test)
-    print predicted
-    # НЕ СОХРАНЯЕТ!!!
-    pred = predicted
-    # report = classification_report(y_test, pred)
-
-    # print report
-print adult_pred
+"""
+# Вероятностная классификация (причём внутри вер-сти [Not_cat, cat])
+# predicted = clf.predict_proba(X_test)
+# print predicted
+# Заменяем содержимое списка на предсказанные значения
+# pred[0:-1] = predicted
